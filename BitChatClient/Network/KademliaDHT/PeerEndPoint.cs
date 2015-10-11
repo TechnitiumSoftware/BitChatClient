459acb4bf1e531dce0df29577c745bcc0a5f8efa
Shreyas Zare <shreyas@technitium.com>
2015-10-11 13:29:21 +0530

    DHT: PeerEndPoint changes done to inherit IPEndPoint for allowing typecasting to IPEndPoint for easy usage.

--- BitChatClient/Network/KademliaDHT/PeerEndPoint.cs
+++ BitChatClient/Network/KademliaDHT/PeerEndPoint.cs
@@ -20,58 +20,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
-using TechnitiumLibrary.IO;
 
 namespace BitChatClient.Network.KademliaDHT
 {
-    public class PeerEndPoint : WriteStream
+    class PeerEndPoint : IPEndPoint
     {
         #region variables
 
         const int PEER_EXPIRY_TIME_SECONDS = 900; //15 min expiry
 
-        DateTime _dateAdded;
-        IPEndPoint _peerEP;
+        DateTime _dateAdded = DateTime.UtcNow;
 
         #endregion
 
         #region constructor
 
+        public PeerEndPoint(IPAddress address, int port)
+            : base(address, port)
+        { }
+
         public PeerEndPoint(Stream s)
+            : base(0, 0)
         {
-            byte[] address;
-            byte[] port;
+            IPEndPoint ep = IPEndPointParser.Parse(s);
 
-            switch (s.ReadByte())
-            {
-                case 0:
-                    address = new byte[4];
-                    port = new byte[2];
-
-                    OffsetStream.StreamRead(s, address, 0, 4);
-                    OffsetStream.StreamRead(s, port, 0, 2);
-                    break;
-
-                case 1:
-                    address = new byte[16];
-                    port = new byte[2];
-
-                    OffsetStream.StreamRead(s, address, 0, 16);
-                    OffsetStream.StreamRead(s, port, 0, 2);
-                    break;
-
-                default:
-                    throw new Exception("AddressFamily not supported.");
-            }
-
-            _peerEP = new IPEndPoint(new IPAddress(address), BitConverter.ToUInt16(port, 0));
-        }
-
-        public PeerEndPoint(IPEndPoint peerEP)
-        {
-            _dateAdded = DateTime.UtcNow;
-            _peerEP = peerEP;
+            this.Address = ep.Address;
+            this.Port = ep.Port;
         }
 
         #endregion
@@ -88,52 +62,9 @@ namespace BitChatClient.Network.KademliaDHT
             _dateAdded = DateTime.UtcNow;
         }
 
-        public override void WriteTo(Stream s)
+        public void WriteTo(Stream s)
         {
-            byte[] address = _peerEP.Address.GetAddressBytes();
-            byte[] port = BitConverter.GetBytes(Convert.ToUInt16(_peerEP.Port));
-
-            switch (_peerEP.AddressFamily)
-            {
-                case AddressFamily.InterNetwork:
-                    s.WriteByte(0);
-                    break;
-
-                case AddressFamily.InterNetworkV6:
-                    s.WriteByte(1);
-                    break;
-
-                default:
-                    throw new Exception("AddressFamily not supported.");
-            }
-
-            s.Write(address, 0, address.Length);
-            s.Write(port, 0, 2);
-        }
-
-        public override void WriteTo(BinaryWriter bW)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool Equals(object obj)
-        {
-            PeerEndPoint peer = obj as PeerEndPoint;
-
-            if (peer == null)
-                return false;
-
-            return _peerEP.Equals(peer._peerEP);
-        }
-
-        public override int GetHashCode()
-        {
-            return _peerEP.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return _peerEP.ToString();
+            IPEndPointParser.WriteTo(this, s);
         }
 
         #endregion
@@ -142,9 +73,6 @@ namespace BitChatClient.Network.KademliaDHT
 
         public DateTime DateAdded
         { get { return _dateAdded; } }
-
-        public IPEndPoint PeerEP
-        { get { return _peerEP; } }
 
         #endregion
     }
