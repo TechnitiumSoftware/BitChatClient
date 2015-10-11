ef37ce017947f91a09ab11404004079bd03b9bf0
Shreyas Zare <shreyas@technitium.com>
2015-10-11 13:42:01 +0530

    BitChatApp: network info display form added.

---
+++ BitChatApp/frmNetworkInfo.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BitChatApp
+{
+    partial class frmNetworkInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNetworkInfo));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(460, 208);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 300;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(397, 230);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmNetworkInfo
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmNetworkInfo";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Network Info";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmNetworkInfo_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
---
+++ BitChatApp/frmNetworkInfo.cs
@@ -0,0 +1,86 @@
+﻿using BitChatClient.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BitChatApp
+{
+    public partial class frmNetworkInfo : Form
+    {
+        INetworkInfo _info;
+        Timer _updateTimer;
+
+        public frmNetworkInfo()
+        {
+            InitializeComponent();
+        }
+
+        public frmNetworkInfo(INetworkInfo info)
+        {
+            InitializeComponent();
+
+            _info = info;
+
+            listView1.Items.Add("Local Peer ID").SubItems.Add("Loading...");
+            listView1.Items.Add("Local Port").SubItems.Add("Loading...");
+            listView1.Items.Add("DHT Node ID").SubItems.Add("Loading...");
+            listView1.Items.Add("DHT Port").SubItems.Add("Loading...");
+            listView1.Items.Add("DHT Nodes").SubItems.Add("Loading...");
+            listView1.Items.Add("Internet Status").SubItems.Add("Loading...");
+            listView1.Items.Add("UPnP Status").SubItems.Add("Loading...");
+            listView1.Items.Add("UPnP External IP").SubItems.Add("Loading...");
+            listView1.Items.Add("External End Point").SubItems.Add("Loading...");
+            listView1.Items.Add("Proxy Nodes").SubItems.Add("Loading...");
+
+            _updateTimer = new Timer();
+            _updateTimer.Interval = 2000;
+            _updateTimer.Tick += _updateTimer_Tick;
+            _updateTimer.Start();
+        }
+
+        private void _updateTimer_Tick(object sender, EventArgs e)
+        {
+            listView1.Items[0].SubItems[1].Text = _info.LocalPeerID.ToString();
+            listView1.Items[1].SubItems[1].Text = _info.LocalPort.ToString();
+            listView1.Items[2].SubItems[1].Text = _info.DhtNodeID.ToString();
+            listView1.Items[3].SubItems[1].Text = _info.DhtLocalPort.ToString();
+            listView1.Items[4].SubItems[1].Text = _info.DhtTotalNodes.ToString();
+            listView1.Items[5].SubItems[1].Text = _info.InternetStatus.ToString();
+            listView1.Items[6].SubItems[1].Text = _info.UPnPStatus.ToString();
+
+            if (_info.UPnPExternalIP == null)
+                listView1.Items[7].SubItems[1].Text = "";
+            else
+                listView1.Items[7].SubItems[1].Text = _info.UPnPExternalIP.ToString();
+
+            if (_info.ExternalEP == null)
+                listView1.Items[8].SubItems[1].Text = "";
+            else
+                listView1.Items[8].SubItems[1].Text = _info.ExternalEP.ToString();
+
+            if (_info.ProxyNodes.Length > 0)
+            {
+                string tmp = "";
+
+                foreach (IPEndPoint proxyNodeEP in _info.ProxyNodes)
+                    tmp += ", " + proxyNodeEP.ToString();
+
+                listView1.Items[9].SubItems[1].Text = tmp.Substring(2);
+            }
+            else
+            {
+                listView1.Items[9].SubItems[1].Text = "";
+            }
+        }
+
+        private void frmNetworkInfo_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _updateTimer.Stop();
+        }
+    }
+}
