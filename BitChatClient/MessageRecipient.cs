d081690808323954d0ea2a7ecb71f31e527d6465
ShreyasZare <shreyas@technitium.com>
2016-08-27 12:18:30 +0530

    MessageRecipient: message delivery datetime added.

--- BitChatClient/MessageRecipient.cs
+++ BitChatClient/MessageRecipient.cs
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+using System;
 using System.IO;
 using TechnitiumLibrary.IO;
 
@@ -35,6 +36,7 @@ namespace BitChatClient
         readonly string _name;
 
         MessageRecipientStatus _status = MessageRecipientStatus.Undelivered;
+        DateTime _deliveredOn;
 
         #endregion
 
@@ -54,12 +56,12 @@ namespace BitChatClient
                 case 1:
                     _name = decoder.DecodeNext().GetStringValue();
                     _status = (MessageRecipientStatus)decoder.DecodeNext().GetByteValue();
+                    _deliveredOn = decoder.DecodeNext().GetDateTimeValue();
                     break;
 
                 default:
                     throw new InvalidDataException("Cannot decode data format: version not supported.");
             }
-
         }
 
         #endregion
@@ -69,6 +71,7 @@ namespace BitChatClient
         public void SetDeliveredStatus()
         {
             _status = MessageRecipientStatus.Delivered;
+            _deliveredOn = DateTime.UtcNow;
         }
 
         public void WriteTo(Stream s)
@@ -78,6 +81,7 @@ namespace BitChatClient
             encoder.Encode((byte)1); //version
             encoder.Encode(_name);
             encoder.Encode((byte)_status);
+            encoder.Encode(_deliveredOn);
         }
 
         public byte[] ToArray()
@@ -106,6 +110,9 @@ namespace BitChatClient
 
         public MessageRecipientStatus Status
         { get { return _status; } }
+
+        public DateTime DeliveredOn
+        { get { return _deliveredOn; } }
 
         #endregion
     }
